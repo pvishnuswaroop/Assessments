f627e102042c9006574ab85316b57ef443c73e06
pvishnuswaroop <141606898+pvishnuswaroop@users.noreply.github.com>
2024-10-17 13:43:42 +0530

    Add files via upload

---
+++ LoanManagementSystem/LoanManagementLibrary/util/DBConnUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Data.SqlClient; 
+
+namespace LoanManagementLibrary.util
+{
+    public static class DBConnUtil
+    {
+        public static SqlConnection GetConnection()
+        {
+           
+            string connectionString = DBPropertyUtil.GetPropertyString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open(); 
+            return connection; 
+        }
+    }
+}
+
