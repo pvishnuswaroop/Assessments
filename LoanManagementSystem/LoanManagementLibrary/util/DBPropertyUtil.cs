7cb9aa868d80417bab199fb89e63cb0e03307420
pvishnuswaroop <141606898+pvishnuswaroop@users.noreply.github.com>
2024-10-17 13:39:11 +0530

    Add files via upload

---
+++ LoanManagementSystem/LoanManagementLibrary/util/DBPropertyUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LoanManagementLibrary.util
+{
+    public static class DBPropertyUtil
+    {
+        public static string GetPropertyString()
+        {
+            
+            return "Server=.\\SQLEXPRESS;Database=LoanManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;";
+        }
+    }
+}
